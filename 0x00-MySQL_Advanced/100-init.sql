c465971e79993d51197b8b35c89a77e4a31148e1
FARAHrafah123 <farahrahali9876@gmail.com>
2024-03-21 16:30:56 +0000

    100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-init.sql
@@ -0,0 +1,47 @@
+-- Initial
+DROP TABLE IF EXISTS corrections;
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS projects;
+
+CREATE TABLE IF NOT EXISTS users (
+    id int not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    average_score float default 0,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+    id int not null AUTO_INCREMENT,
+    name varchar(255) not null,
+    weight int default 1,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS corrections (
+    user_id int not null,
+    project_id int not null,
+    score float default 0,
+    KEY `user_id` (`user_id`),
+    KEY `project_id` (`project_id`),
+    CONSTRAINT fk_user_id FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE,
+    CONSTRAINT fk_project_id FOREIGN KEY (`project_id`) REFERENCES `projects` (`id`) ON DELETE CASCADE
+);
+
+INSERT INTO users (name) VALUES ("farah");
+SET @user_farah = LAST_INSERT_ID();
+
+INSERT INTO users (name) VALUES ("kamal");
+SET @user_kamal = LAST_INSERT_ID();
+
+INSERT INTO projects (name, weight) VALUES ("C is fun", 1);
+SET @project_c = LAST_INSERT_ID();
+
+INSERT INTO projects (name, weight) VALUES ("Python is cool", 2);
+SET @project_py = LAST_INSERT_ID();
+
+
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_farah, @project_c, 80);
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_farah, @project_py, 96);
+
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_kamal, @project_c, 91);
+INSERT INTO corrections (user_id, project_id, score) VALUES (@user_kamal, @project_py, 73);
