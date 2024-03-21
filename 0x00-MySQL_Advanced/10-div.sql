24cec51a6093a872104c167032e80a2cbbdd487c
FARAHrafah123 <farahrahali9876@gmail.com>
2024-03-21 16:23:01 +0000

    add files

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,15 +1,13 @@
--- script that creates a function SafeDiv that divides (and returns)
--- the first by the second number or returns 0 if the second number is equal to 0.
-DROP FUNCTION IF EXISTS SafeDiv;
+-- Creates a function SafeDiv that divides (and returns)
+-- the first by the second number or returns 0
+-- if the second number is equal to 0.
+
 DELIMITER $$
-CREATE FUNCTION SafeDiv(a INT, b INT) 
-RETURNS INT AS $$
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
 BEGIN
-    IF b = 0 THEN
-        RETURN 0;
-    ELSE
-        RETURN a / b;
-    END IF;
-END;
+    RETURN (IF (b = 0, 0, a / b));
+END
 $$
-DELIMITER;
+DELIMITER ;
